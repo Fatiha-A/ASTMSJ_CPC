acb68de700f9ba1b5b1e445a8b56b556eefa4c5a
Fatiha-A <145744145+Fatiha-A@users.noreply.github.com>
2025-07-24 22:23:32 +0300

    Time: 690 ms (5.93%) | Memory: 18.9 MB (74.98%) - LeetSync

--- 506-relative-ranks/relative-ranks.py
+++ 506-relative-ranks/relative-ranks.py
@@ -1,21 +1,17 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
+        s_list=sorted(score, reverse=True)
         rank=[]
-        sort=sorted(score, reverse=True)
-        f="Gold Medal"
-        s="Silver Medal"
-        th="Bronze Medal"
+        first="Gold Medal"
+        second="Silver Medal"
+        third="Bronze Medal"
         for i in range(len(score)):
             if score[i]==max(score):
-                rank.append(f)
-            elif score[i]==sort[1]:
-                rank.append(s)
-            elif score[i]==sort[2]:
-                rank.append(th)
+                rank.append(first)
+            elif score[i]==s_list[1]:
+                rank.append(second)
+            elif score[i]==s_list[2]:
+                rank.append(third)
             else:
-                rank.append(str(sort.index(score[i])+1))
-        return rank
-
-
-
-                
+                rank.append(str( s_list.index(score[i]) + 1))
+        return rank   
